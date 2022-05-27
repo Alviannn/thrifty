0b020d19e39e554e1ee3d4ce36ca5886a271717a
leonardolim78 <leonardolim178@gmail.com>
2022-05-27 18:18:10 +0700

    Add product form input component

---
+++ frontend/components/Product/ProductFormInput.jsx
@@ -0,0 +1,21 @@
+import { useContext } from 'react';
+import { FormInputContext } from './ProductForm';
+
+const ProductFormInput = ({ propKey, name, icon }) => {
+    const { data, setData, errors } = useContext(FormInputContext);
+
+    const handleChange = (e) => {
+        const { value } = e.target;
+        setData({ ...data, [propKey]: value });
+    };
+
+    return (
+        <div className="form-group mb-3">
+            <span className="me-2 mt-1 position-absolute text-mocca">{icon}</span>
+            <input type="text" className="form-control bg-transparent w-75 ps-4 border-0 border-bottom rounded-0 d-inline text-dark-brown" name={propKey} placeholder={`Masukkan ${name} produk`} value={data[propKey]} onChange={handleChange} />
+            {errors[propKey] && <div className="alert alert-warning w-75 py-2 mb-2 m-auto float-lg-start text-start" role="alert">{errors[propKey]}</div>}
+        </div>
+    );
+};
+
+export default ProductFormInput;
