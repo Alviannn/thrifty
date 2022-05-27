740a27e9944c3a7a0433b2af1b80350bd1c192f9
leonardolim78 <leonardolim178@gmail.com>
2022-05-27 18:16:53 +0700

    Add buttons for sorting, filtering, and add product

--- frontend/pages/products/index.jsx
+++ frontend/pages/products/index.jsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import useSWR from 'swr';
 import axios from 'axios';
+import { FaArrowDown, FaArrowUp, FaPlus } from 'react-icons/fa';
 import FetchLoading from '../../components/Widget/FetchLoading';
 import ProductItem from '../../components/Product/ProductItem';
 
@@ -30,7 +32,28 @@ const Product = () => {
                 {!items ? (
                     <FetchLoading />
                 ) : (
-                    <div className="row mt-lg-5 pt-5 d-flex justify-content-center text-black">
+                    <div className="row pt-5 d-flex justify-content-center text-black">
+                        <div className="col-4 mb-4">
+                            <h5>Urut berdasarkan</h5>
+                            <button className="btn btn-sm btn-brown me-2"><FaArrowDown /> Harga Terendah</button>
+                            <button className="btn btn-sm btn-dark-brown me-2"><FaArrowUp /> Harga Tertinggi</button>
+                        </div>
+
+                        <div className="col-4 mb-4">
+                            <h5>Filter berdasarkan</h5>
+                            <button className="btn btn-sm btn-beige me-2">Atasan</button>
+                            <button className="btn btn-sm btn-mocca me-2">Bawahan</button>
+                            <button className="btn btn-sm btn-beige me-2">Luaran</button>
+                            <button className="btn btn-sm btn-mocca me-2">Lainnya</button>
+                        </div>
+
+                        <div className="col-4 mb-4">
+                            <h5>Masukkan produk anda sendiri</h5>
+                            <Link href="/products/add">
+                                <a className="btn btn-sm btn-brown"><FaPlus /> Tambah Produk</a>
+                            </Link>
+                        </div>
+
                         {items.map((item) => {
                             return (
                                 <div key={item.id} className="col-lg-4 col-md-6 col-12 p-0">
