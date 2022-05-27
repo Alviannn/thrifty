37b861a68b7b4644278311cc654888878b7f77fb
leonardolim78 <leonardolim178@gmail.com>
2022-05-27 16:39:47 +0700

    Add background to layout

--- frontend/components/Layout/Layout.jsx
+++ frontend/components/Layout/Layout.jsx
@@ -1,9 +1,11 @@
+import Background from "./Background";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 const Layout = ({ children }) => {
 	return (
 		<>
+            <Background />
 			<Navbar />
 			<div class="container">
 				<main>{children}</main>
